9f89c434e7382ffafefbd981c14fb8862db87362
tiagopog <tiagopog@gmail.com>
2026-02-25 20:04:25 -0300

    test: add support to ESM with Jest

--- src/tests/jest.config.js
+++ src/tests/jest.config.js
@@ -1,11 +1,12 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 
-module.exports = {
-  preset: 'ts-jest',
+export default {
+  preset: 'ts-jest/presets/default-esm',
   rootDir: './',
   testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
   transform: {
-    '^.+.tsx?$': ['ts-jest', {}],
+    '^.+\\.tsx?$': ['ts-jest', { useESM: true }],
   },
   setupFiles: ['<rootDir>/jest.envs.ts'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
--- src/tests/jest.envs.ts
+++ src/tests/jest.envs.ts
@@ -1,5 +1,8 @@
 import path from 'path'
+import { fileURLToPath } from 'url'
 import * as dotenv from 'dotenv'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 dotenv.config({
   path: path.resolve(__dirname, `../../.env.test`),
